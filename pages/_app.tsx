64698832417e7915ae2b146fc3109a5e51ae105a
Brenner Spear <blspear@gmail.com>
2022-07-29 13:38:38 -0400

    new font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,8 @@ import type { AppProps } from 'next/app'
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
 
-import { ChakraProvider, extendTheme, Flex } from '@chakra-ui/react'
-import '@fontsource/courier-prime'
-import '@fontsource/lato'
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { ChakraProvider, Flex } from '@chakra-ui/react'
+import { darkTheme, RainbowKitProvider, Theme } from '@rainbow-me/rainbowkit'
 import '@rainbow-me/rainbowkit/styles.css'
 import '@rainbow-me/rainbowkit/styles.css'
 import { WagmiConfig } from 'wagmi'
@@ -19,6 +17,10 @@ import { theme } from '../styles/theme'
 
 const bgSize = ['100px', '120px', '220px', '300px']
 
+const customTheme: Theme = darkTheme()
+
+customTheme.fonts.body = 'Lars'
+
 function App({ Component, pageProps }: AppProps): JSX.Element {
     const { route } = useRouter()
 
@@ -27,7 +29,7 @@ function App({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <ChakraProvider theme={theme}>
             <WagmiConfig client={wagmiClient}>
-                <RainbowKitProvider chains={chains}>
+                <RainbowKitProvider chains={chains} theme={customTheme}>
                     <Flex bgColor="brand.100opaque" width="100%" minH="100vh">
                         <Layout>
                             <Component {...pageProps} />

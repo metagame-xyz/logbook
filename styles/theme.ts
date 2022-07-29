8af9360615bf1e9ad893b0a70f64647db5aa512c
leojacoby <leojacoby@gmail.com>
2022-07-29 18:05:50 -0400

    Revert "Merge branch 'dev' into leo-building"
    This reverts commit c3bb9ed6c2c92b231d26395dc273e2ee2f0d38d1, reversing
    changes made to af9ee18f986d37966a7a8abbe9830c08bda038be.

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,11 +1,9 @@
-import { extendTheme } from '@chakra-ui/react'
-
-export const theme = extendTheme({
-    styles: {
-        global: {
-            'html, body': {
-                color: 'brand.900',
-            },
+const theme = {
+    global: {
+        font: {
+        family: 'Lars',
+        size: '18px',
+        height: '20px',
         },
         colors: {
             backgroundDark: '#163D26',
@@ -13,24 +11,6 @@ export const theme = extendTheme({
             brand: '#C84414',
         }
     },
-    colors: {
-        white: '#F7FAFC',
-        brand: {
-            '100opaque': 'rgba(237, 242, 247, 0.92)',
-            '50': '#F7FAFC',
-            '100': '#EDF2F7',
-            '200': '#E2E8F0',
-            '300': '#CBD5E0',
-            '400': '#A0AEC0',
-            '500': '#718096',
-            '600': '#4A5568',
-            '700': '#2D3748',
-            '800': '#1A202C',
-            '900': '#171923',
-        },
-    },
-    fonts: {
-        heading: 'Lars',
-        body: 'Lars',
-    },
-})
+};
+
+export default theme

98b8337ab27518424eb9f621e3e937dcb228cc83
Brenner Spear <blspear@gmail.com>
2022-07-31 14:46:55 -0400

    fwb sentences

--- utils/createSentences.ts
+++ utils/createSentences.ts
@@ -485,9 +485,22 @@ export default function createSentences(interpretedData: (Interpretation | null)
 
     // bridged (need contract-specific first)
 
+    const uniqueSentences = []
+
     // issued fwb after the hack: https://etherscan.io/tx/0xc010aa0eda23ccd89a6bedf967652c1d8dfed9cc9d2ee02e9744e36a0392e409
+    const postHackTx = filteredData.find((tx) => {
+        tx.txHash === '0xc010aa0eda23ccd89a6bedf967652c1d8dfed9cc9d2ee02e9744e36a0392e409'
+    })
+    if (postHackTx) {
+        uniqueSentences.push(`was re-issued $FWB after the March '21 hack`)
+    }
 
     // paid by fwb: https://etherscan.io/tx/0xc840415316bc7ba172362117d4015babc9f9e0626746fca181f2b3f7c3b12c0d
+    const fwbAddresses = ['0x660f6d6c9bcd08b86b50e8e53b537f2b40f243bd', '0x33e626727b9ecf64e09f600a1e0f5adde266a0df']
+    const paidByFwb = filteredData.find((tx) => fwbAddresses.includes(tx.contractAddress))
+    if (paidByFwb) {
+        uniqueSentences.push(`was paid by FWB for being a contributor`)
+    }
 
     // const unknownTxs = []
     // for (const action of actions) {

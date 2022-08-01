eb9e4e7d823c234398ef8aa75aef12eb6fc4e16e
Brenner Spear <blspear@gmail.com>
2022-08-01 18:47:42 -0400

    fix

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,47 +1,49 @@
-const theme = {
+import { ThemeType } from 'grommet'
+
+const theme: ThemeType = {
     button: {
         default: {
             border: {
-                radius: '0px',
-                color: 'brand'
+                // radius: '0px',
+                color: 'brand',
             },
             background: {
-                color: 'backgroundLight'
+                color: 'backgroundLight',
             },
-            color: 'brand'
+            color: 'brand',
         },
         primary: {
             border: {
-                radius: '0px'
+                // radius: '0px',
             },
             background: {
-                color: '#000000'
-            }
+                color: '#000000',
+            },
         },
         secondary: {
             border: {
-                radius: '0px',
+                // radius: '0px',
                 color: 'brand',
-                width: '1px'
+                width: '1px',
             },
-            color: 'brand'
+            color: 'brand',
         },
         border: {
-            radius: '0px'
+            radius: '0px',
         },
     },
     global: {
         font: {
-        family: 'Lars',
-        size: '18px',
-        height: '20px',
+            family: 'Lars',
+            size: '18px',
+            height: '20px',
         },
         colors: {
             backgroundDark: '#163D26',
             backgroundLight: '#FBF7F1',
             brand: '#C84414',
-        }
+        },
     },
-};
+}
 
 export default theme

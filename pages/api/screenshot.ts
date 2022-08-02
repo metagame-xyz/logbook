4f7cf6a79346d41e9e5c9a66d622281fa985499c
Brenner Spear <blspear@gmail.com>
2022-08-02 19:37:32 -0400

    ok node14

--- pages/api/screenshot.ts
+++ pages/api/screenshot.ts
@@ -4,8 +4,10 @@ import chrome from 'chrome-aws-lambda'
 import puppeteer from 'puppeteer-core'
 
 const api: NextApiHandler = async (req, res) => {
-    const url = `https://${process.env.VERCEL_URL}/_generators/shield-image?field=${req.query.field}&colors=${req.query.colors}&hardware=${req.query.hardware}&frame=${req.query.frame}`
-    const image = await getScreenshotOfURL(url)
+    const { url, height, width } = req.query as { url: string; height: string; width: string }
+
+    // const url = `https://${process.env.VERCEL_URL}/_generators/shield-image?field=${req.query.field}&colors=${req.query.colors}&hardware=${req.query.hardware}&frame=${req.query.frame}`
+    const image = await getScreenshotOfURL(url, parseInt(height), parseInt(width))
     res.setHeader('Content-Type', 'image/png')
     res.setHeader('Cache-Control', 'public, max-age=31536000, s-max-age=31536000')
     return res.send(image)
@@ -13,14 +15,14 @@ const api: NextApiHandler = async (req, res) => {
 
 export default api
 
-const getScreenshotOfURL = async (url: string) => {
+const getScreenshotOfURL = async (url: string, height: number, width: number) => {
     const browser = await puppeteer.launch({
         args: chrome.args,
         executablePath: await chrome.executablePath,
         headless: chrome.headless,
         defaultViewport: {
-            width: 1100,
-            height: 1320,
+            width,
+            height,
             deviceScaleFactor: 2,
         },
     })
